f84f1a73f862ed6a8a65df62e9d421c1669a0707
DanielB <d.banasiewicz@pdo.com.pl>
2021-09-19 19:51:05 +0200

    html structure is done; styles created; script in main.js worked and done;

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("button");
+let number = 1;
+const addElement = function () {
+    // console.log(`click!`);
+    const div = document.createElement("div");
+    div.textContent = number;
+    if (number % 5 != 0) {
+        document.body.appendChild(div);
+    } else {
+        document.body.appendChild(div).classList.add("circle");
+    }
+    number++;
+    // console.log(div);
+}
+btn.addEventListener("click", addElement)
